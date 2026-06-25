dd6662d03ef47ade6092889574acef213cb42842
Priya Kumari <priya462005kumari@gmail.com>
2026-06-25 15:31:58 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
+
+
+
+app.get("/", (req, res) => {
+    res.send("Ledger Service is up and running")
+})
+
+app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
+app.use("/api/transactions", transactionRoutes)
+
+module.exports = app
